30765266786c5f54d65100deb1cb7e7fefb757f4
Wilsonw30 <90718167+Wilsonw30@users.noreply.github.com>
2022-03-21 13:57:23 -0400

    awd

---
+++ .elevety.js
@@ -0,0 +1,10 @@
+module.exports = function(elventyConfig){
+    elventyConfig.addPassthroughCopy("src/style")
+    elventyConfig.addPassthroughCopy("src/img")
+    return{
+        dir:{
+            input:"src",
+            output:"dist",
+        }
+    }
+}
